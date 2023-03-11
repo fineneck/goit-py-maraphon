3c91ce5e1f8658522c0ec6767e5ffd14f36c7ba9
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-11 15:10:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ pygame.init()
 
 screen = width, heigth = 800, 600
 
-TOMATO = 255, 99, 71
-TEAL = 0, 128, 128
+RED = 255, 0, 0
+GREEN = 0, 255, 0
 BLACK = 0, 0, 0,
+WHITE = 255, 255, 255
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
-ball_fill = ball.fill(TEAL)
+ball_fill = ball.fill(WHITE)
 ball_rect = ball.get_rect()
 ball_speed = [1, 1]
 
@@ -25,34 +26,33 @@ while is_working:
   
   ball_rect = ball_rect.move(ball_speed)
 
-  if ball_rect.bottom >= heigth or ball_rect.top <= 0:
+  # if ball_rect.bottom >= heigth or ball_rect.top <= 0:
+  #   ball_speed[1] = -ball_speed[1]
+  #   ball.fill(RED)
+  
+  # if ball_rect.left <= 0 or ball_rect.right >= width:
+  #   ball_speed[0] = -ball_speed[0]
+  #   ball.fill(GREEN)
+
+  if ball_rect.bottom >= heigth:
     ball_speed[1] = -ball_speed[1]
     ball.fill(random.sample(range(255), 3))
   
-  if ball_rect.left <= 0 or ball_rect.right >= width:
+  if ball_rect.top <= 0:
+    ball_speed[1] = -ball_speed[1]
+    ball.fill(random.sample(range(255), 3))
+  
+  if ball_rect.left <= 0:
     ball_speed[0] = -ball_speed[0]
     ball.fill(random.sample(range(255), 3))
 
-  # if ball_rect.bottom >= heigth:
-  #   ball_speed[1] = -ball_speed[1]
-  #   ball.fill(TOMATO)
+  if ball_rect.right >= width:
+    ball_speed[0] = -ball_speed[0]
+    ball.fill(random.sample(range(255), 3))
   
-  # if ball_rect.top <= 0:
-  #   ball_speed[1] = -ball_speed[1]
-  #   ball.fill(TOMATO)
-  
-  # if ball_rect.left <= 0:
-  #   ball_speed[0] = -ball_speed[0]
-  #   ball.fill(TEAL)
-
-  # if ball_rect.right >= width:
-  #   ball_speed[0] = -ball_speed[0]
-  #   ball.fill(TEAL)
-
 
   main_surface.fill(BLACK)
   main_surface.blit(ball, ball_rect)
-
 
   pygame.display.flip()
 
